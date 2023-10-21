3d82af8521612f48c131916b4574ff8838a3331e
Ildar Zagidullin <zigzagildar@gmail.com>
2023-10-21 20:12:54 +0000

    my_terraform_and_ansible

--- TwoServers/main.tf
+++ TwoServers/main.tf
@@ -8,9 +8,9 @@ terraform {
 }
 
 provider "yandex" {
-  token     = "<--->"
-  cloud_id  = "<--->"
-  folder_id = "<--->"
+  token     = "y0_AgAAAABxd_yIAATuwQAAAADvUBPa-hrDRed3ScCYxm-Cu8Ezz9-Ekhs"
+  cloud_id  = "b1gkp4p2fc5m4tk4mv23"
+  folder_id = "b1gk91seujvu5ushi47l"
   zone      = "ru-central1-a"
 }
 
