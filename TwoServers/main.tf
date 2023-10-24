d28e086d632c67a62b600bdfdb2a6faedf54358e
Ildar Zagidullin <zigzagildar@gmail.com>
2023-10-24 00:21:22 +0000

    Update main.tf

--- TwoServers/main.tf
+++ TwoServers/main.tf
@@ -8,10 +8,10 @@ terraform {
 }
 
 provider "yandex" {
-  token     = "y0_AgAAAABxd_yIAATuwQAAAADvUBPa-hrDRed3ScCYxm-Cu8Ezz9-Ekhs"
-  cloud_id  = "b1gkp4p2fc5m4tk4mv23"
-  folder_id = "b1gk91seujvu5ushi47l"
-  zone      = "ru-central1-a"
+  token     = var.token
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
+  zone      = var.zone
 }
 
 data "yandex_compute_image" "my-ubuntu-2004-1" {
